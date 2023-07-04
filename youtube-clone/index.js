98e4401709d053dc74fc70eaa71e296bf713befb
yashdkadam <kadamdyash55@gmail.com>
2023-07-04 15:34:12 +0530

    fixes

--- youtube-clone/index.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('#menu');
-console.log(menu);
-
-const sidebar = document.querySelector('.sidebar');
-
-menu.addEventListener('click', function () {
-  sidebar.classList.toggle('show-sidebar');
-});
